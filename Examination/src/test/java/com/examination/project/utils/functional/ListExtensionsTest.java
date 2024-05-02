c381797765dcb63fecb0b6341306cde8051f9e14
Mohsen DAHESH <mdehesh@gmail.com>
2024-05-02 18:50:23 +0200

    add unit test class for NullUtils and ListExtentions utils

---
+++ Examination/src/test/java/com/examination/project/utils/functional/ListExtensionsTest.java
@@ -0,0 +1,29 @@
+package com.examination.project.utils.functional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListExtensionsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void toZip() {
+    }
+
+    @Test
+    void peek() {
+    }
+
+    @Test
+    void filter() {
+    }
+
+    @Test
+    void map() {
+    }
+}
---
+++ Examination/src/test/java/com/examination/project/utils/functional/NullUtilsTest.java
@@ -0,0 +1,77 @@
+package com.examination.project.utils.functional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NullUtilsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void acceptIfNotNull() {
+    }
+
+    @Test
+    void testAcceptIfNotNull() {
+    }
+
+    @Test
+    void acceptNotNull() {
+    }
+
+    @Test
+    void ifConditionAndNotNull() {
+    }
+
+    @Test
+    void ifNotNull() {
+    }
+
+    @Test
+    void testIfNotNull() {
+    }
+
+    @Test
+    void ifNotNullOr() {
+    }
+
+    @Test
+    void testAcceptIfNotNull1() {
+    }
+
+    @Test
+    void acceptIfNot() {
+    }
+
+    @Test
+    void testAcceptIfNotNull2() {
+    }
+
+    @Test
+    void testAcceptIfNotNull3() {
+    }
+
+    @Test
+    void findFirst() {
+    }
+
+    @Test
+    void eval() {
+    }
+
+    @Test
+    void emptyStringIfNull() {
+    }
+
+    @Test
+    void nullIfEmptyString() {
+    }
+
+    @Test
+    void evalOrEmpty() {
+    }
+}
