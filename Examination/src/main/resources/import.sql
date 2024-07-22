af23fa8a4af2ab5cd48d0ba5db98b827c6f21f1b
Mohsen Dahesh <mdehesh@gmail.com>
2024-07-22 19:02:03 +0200

    add more sql insert for subject entity

--- Examination/src/main/resources/import.sql
+++ Examination/src/main/resources/import.sql
@@ -27,6 +27,8 @@ INSERT INTO subjects
 VALUES (2205, 180, null, null, null, null, 'MODULE_1', 'Music') ON CONFLICT DO NOTHING;
 INSERT INTO subjects
 VALUES (2206, 182, null, null, null, null, 'MODULE_2', 'Chemistry') ON CONFLICT DO NOTHING;
+INSERT INTO subjects
+VALUES (2207, 190, null, null, null, null, 'MODULE_2', 'Chemistry') ON CONFLICT DO NOTHING;
 
 INSERT INTO students
 VALUES (800, '2002-04-25T13:22:09.389365700Z', 'classeA', 'Alex', 45654654, 'BERNARD', 3) ON CONFLICT DO NOTHING;
