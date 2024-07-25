bd97fb9253d5a0485b259941f33f1691e6d1bee9
Mohsen Dahesh <mdehesh@gmail.com>
2024-07-25 14:46:41 +0200

    replace Mock and InjectMocks annotations by static import and constructure injection

--- Examination/src/test/java/com/examination/project/infrastructure/handler/controller/IntegrationTest.java
+++ Examination/src/test/java/com/examination/project/infrastructure/handler/controller/IntegrationTest.java
@@ -7,26 +7,30 @@ import com.examination.project.domain.usecases.v1.room.RoomUseCase;
 import com.examination.project.domain.usecases.v1.student.StudentUseCase;
 import com.examination.project.domain.usecases.v1.subject.SubjectUseCase;
 import com.examination.project.infrastructure.config.web.ObjectMapperConfiguration;
+import com.examination.project.infrastructure.handler.controller.v1.exam.ExamHandler;
 import com.examination.project.infrastructure.handler.controller.v1.exam.ExamRestHandler;
 import com.examination.project.infrastructure.handler.controller.v1.exam.fixture.ExamRestHandlerFixture;
+import com.examination.project.infrastructure.handler.controller.v1.invigilator.InvigilatorHandler;
 import com.examination.project.infrastructure.handler.controller.v1.invigilator.InvigilatorRestHandler;
 import com.examination.project.infrastructure.handler.controller.v1.invigilator.fixture.InvigilatorRestHandlerFixture;
+import com.examination.project.infrastructure.handler.controller.v1.room.RoomHandler;
 import com.examination.project.infrastructure.handler.controller.v1.room.RoomRestHandler;
 import com.examination.project.infrastructure.handler.controller.v1.room.fixture.RoomRestHandlerFixture;
+import com.examination.project.infrastructure.handler.controller.v1.student.StudentHandler;
 import com.examination.project.infrastructure.handler.controller.v1.student.StudentRestHandler;
 import com.examination.project.infrastructure.handler.controller.v1.student.fixture.StudentRestHandlerFixture;
+import com.examination.project.infrastructure.handler.controller.v1.subject.SubjectHandler;
 import com.examination.project.infrastructure.handler.controller.v1.subject.SubjectRestHandler;
 import com.examination.project.infrastructure.handler.controller.v1.subject.fixture.SubjectRestHandlerFixture;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
@@ -37,8 +41,6 @@ public abstract class IntegrationTest {
 
     protected ObjectMapper objectMapper;
 
-    protected SubjectRestHandlerFixture subjectRestHandlerFixture;
-
     protected ExamRestHandlerFixture examRestHandlerFixture;
 
     protected InvigilatorRestHandlerFixture invigilatorRestHandlerFixture;
@@ -47,54 +49,46 @@ public abstract class IntegrationTest {
 
     protected StudentRestHandlerFixture studentRestHandlerFixture;
 
-    @Mock
-    protected InvigilatorUseCase invigilatorUseCaseMocked;
+    protected SubjectRestHandlerFixture subjectRestHandlerFixture;
 
-    @InjectMocks
-    protected InvigilatorRestHandler invigilatorRestHandler;
+    protected ExamUseCase examUseCaseMocked = mock(ExamUseCase.class);
 
-    @Mock
-    protected RoomUseCase roomUseCaseMocked;
+    protected InvigilatorUseCase invigilatorUseCaseMocked = mock(InvigilatorUseCase.class);
 
-    @InjectMocks
-    protected RoomRestHandler roomRestHandler;
+    protected RoomUseCase roomUseCaseMocked = mock(RoomUseCase.class);
 
-    @Mock
-    protected ExamUseCase examUseCaseMocked;
+    protected StudentUseCase studentUseCaseMocked = mock(StudentUseCase.class);
 
-    @InjectMocks
-    protected ExamRestHandler examRestHandler;
+    protected SubjectUseCase subjectUseCaseMocked = mock(SubjectUseCase.class);
 
-    @Mock
-    protected SubjectUseCase subjectUseCaseMocked;
+    protected ExamHandler examRestHandler = new ExamRestHandler(examUseCaseMocked);
 
-    @InjectMocks
-    protected SubjectRestHandler subjectRestHandler;
+    protected InvigilatorHandler invigilatorRestHandler = new InvigilatorRestHandler(invigilatorUseCaseMocked);
 
-    @Mock
-    protected StudentUseCase studentUseCaseMocked;
+    protected RoomHandler roomRestHandler = new RoomRestHandler(roomUseCaseMocked);
 
-    @InjectMocks
-    protected StudentRestHandler studentRestHandler;
+    protected StudentHandler studentRestHandler = new StudentRestHandler(studentUseCaseMocked);
+
+    protected SubjectHandler subjectRestHandler = new SubjectRestHandler(subjectUseCaseMocked);
 
     @BeforeEach
     public void setUp() {
         this.mockMvc = standaloneSetup(
                 examRestHandler,
-                subjectRestHandler,
                 invigilatorRestHandler,
                 roomRestHandler,
-                studentRestHandler
+                studentRestHandler,
+                subjectRestHandler
         ).setControllerAdvice(new GlobalExceptionHandler())
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .build();
 
         this.objectMapper = new ObjectMapperConfiguration().objectMapper();
 
-        this.subjectRestHandlerFixture = SubjectRestHandlerFixture.from(this.mockMvc, this.objectMapper);
         this.examRestHandlerFixture = ExamRestHandlerFixture.from(this.mockMvc, this.objectMapper);
         this.invigilatorRestHandlerFixture = InvigilatorRestHandlerFixture.from(this.mockMvc, this.objectMapper);
         this.roomRestHandlerFixture = RoomRestHandlerFixture.from(this.mockMvc, this.objectMapper);
         this.studentRestHandlerFixture = StudentRestHandlerFixture.from(this.mockMvc, this.objectMapper);
+        this.subjectRestHandlerFixture = SubjectRestHandlerFixture.from(this.mockMvc, this.objectMapper);
     }
 }
